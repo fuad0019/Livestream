b83ae7091cc666b883499b3457ce6ae239e8c833
Fuad <fujam20@student.sdu.dk>
2022-09-01 22:34:50 +0200

    added propeller meta data

--- live-stream-part-3/auth/server.js
+++ live-stream-part-3/auth/server.js
@@ -8,7 +8,7 @@ app.post("/auth", function (req, res) {
   const streamkey = req.body.key;
 
   /* You can make a database of users instead :) */
-  if (streamkey === "fuad0019") {
+  if (streamkey === "supersecret") {
     res.status(200).send();
     return;
   }
