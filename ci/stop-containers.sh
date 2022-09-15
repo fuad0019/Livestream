f03d0f8e3f42a6b98bf789162a408b2e008e54e6
Fuad <fujam20@student.sdu.dk>
2022-09-15 21:17:26 +0200

    added master instead of main in workflow file

--- ci/stop-containers.sh
+++ ci/stop-containers.sh
@@ -1,6 +1,21 @@
-if [[ $(docker container ls) ]]; then
-    docker stop $(docker ps -a -q)
-    docker rm $(docker ps -a -q)
-else
-    echo "no containers"
+if [ ! "$(docker ps -q -f name=rtmp_server)" ]; then
+        # cleanup
+    docker stop rtmp_server
+    docker rm rtmp_server
+
+fi
+
+if [ ! "$(docker ps -q -f name=frontend)" ]; then
+        # cleanup
+    docker stop frontend
+    docker rm frontend
+
+fi
+
+
+if [ ! "$(docker ps -q -f name=auth)" ]; then
+        # cleanup
+    docker stop auth
+    docker rm auth
+
 fi
